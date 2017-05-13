7f5494fae1388a2dfb242b6a2627beb97543e143
ivan <qwIvan@users.noreply.github.com>
2017-05-13 22:30:20 +0800

    修复被at的问题

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import meme
+import re
 from wxpy import *
 from functools import lru_cache
 # bot = Bot(cache_path='wx_session', console_qr=True)
@@ -19,14 +20,18 @@ searched = {}
 
 @bot.register(except_self=False)
 def accept(msg):
-    if msg.is_at or msg.sender == bot.self and msg.text.endswith('.gif'):
+    if msg.is_at:
+        keyword = re.sub('@\S+', '', msg.text)
+    elif msg.sender == bot.self and msg.text.endswith('.gif'):
         keyword = msg.text[:-4]
-        imgs = keyword in searched and searched[keyword]
-        if not imgs:
-            imgs = meme.search(keyword)
-            searched[keyword] = imgs
-        media_id = gif_media_id(*imgs.pop(0))
-        msg.reply_image('.gif', media_id=media_id)
+    else:
+        return
+    imgs = keyword in searched and searched[keyword]
+    if not imgs:
+        imgs = meme.search(keyword)
+        searched[keyword] = imgs
+    media_id = gif_media_id(*imgs.pop(0))
+    msg.reply_image('.gif', media_id=media_id)
 
 
 bot.join()
