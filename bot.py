8405cab3922bb95ac733d46cfc91d08d6421dfd7
ivan <qwIvan@users.noreply.github.com>
2017-06-16 05:03:47 +0800

    add .jpg suffix

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import meme
-import re
 from wxpy import *
 from functools import lru_cache
-# bot = Bot(cache_path='wx_session', console_qr=True)
-bot = Bot(console_qr=True)
+bot = Bot(cache_path=True, console_qr=True)
+# bot = Bot(console_qr=True)
 
 
 @lru_cache()
@@ -20,14 +19,15 @@ searched = {}
 
 @bot.register(msg_types=TEXT, except_self=False)
 def accept(msg):
-    if msg.text.endswith('.gif'):
+    if msg.text[-4:] in ('.gif', '.jpg'):
         keyword = msg.text[:-4]
         imgs = keyword in searched and searched[keyword]
         if not imgs:
             imgs = meme.search(keyword)
             searched[keyword] = imgs
-        media_id = gif_media_id(*imgs.pop(0))
-        msg.reply_image('.gif', media_id=media_id)
+        if imgs:
+            media_id = gif_media_id(*imgs.pop(0))
+            msg.reply_image('.gif', media_id=media_id)
 
 
 @bot.register(msg_types=FRIENDS)
