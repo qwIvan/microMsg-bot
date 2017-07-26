4f3144376b28ae5cd85470896e264c6cf7972aa4
ivan <qwIvan@users.noreply.github.com>
2017-07-27 03:09:25 +0800

    在http://bot.libivan.com提供socket.io的登录界面

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 from logger import logger
-from flask import Flask, request
+from flask import Flask, request, url_for
 from bot import EmotionBot, SyncEmotionBot
 from flask_socketio import SocketIO, emit
 
@@ -10,17 +10,18 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def login():
-    def qr_callback(uuid, status, qrcode):
-        logger.info('uuid=%s, status=%s', uuid, status)
-
-    def login_calback():
-        bot.print_cookies()
-
-    try:
-        bot = SyncEmotionBot(qr_callback=qr_callback, timeout_max=15, login_callback=login_calback)
-    except EmotionBot.TimeoutException as e:
-        logger.warning('uuid=%s, status=%s, timeout', e.uuid, e.status)
-    return '<img src=http://login.weixin.qq.com/qrcode/%s >' % bot.uuid
+    return app.send_static_file('index.html')
+#     def qr_callback(uuid, status, qrcode):
+#         logger.info('uuid=%s, status=%s', uuid, status)
+#
+#     def login_calback():
+#         bot.print_cookies()
+#
+#     try:
+#         bot = SyncEmotionBot(qr_callback=qr_callback, timeout_max=15, login_callback=login_calback)
+#     except EmotionBot.TimeoutException as e:
+#         logger.warning('uuid=%s, status=%s, timeout', e.uuid, e.status)
+#     return '<img src=http://login.weixin.qq.com/qrcode/%s >' % bot.uuid
 
 
 @socketio.on('login')
