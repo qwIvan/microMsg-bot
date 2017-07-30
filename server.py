8ce695354d695ee9713fc147aaabf5c7246e94ba
ivan <qwIvan@users.noreply.github.com>
2017-07-31 00:58:45 +0800

    维持同一个session的所有标签页都同一个状态

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 from logger import logger
 from flask import Flask, request, session
 from bot import EmotionBot, SyncEmotionBot
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 app.secret_key = 'test'
@@ -14,8 +14,8 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def login():
-    if 'cache_path' not in session:
-        session['cache_path'] = str(secrets.randbits(256))
+    if 'sessionID' not in session:
+        session['sessionID'] = str(secrets.randbits(256))
     return app.send_static_file('index.html')
 #     def qr_callback(uuid, status, qrcode):
 #         logger.info('uuid=%s, status=%s', uuid, status)
@@ -35,47 +35,49 @@ bots = {}
 @socketio.on('login')
 def login():
 
-    def success_ack(bot, sid, nickname):
-        socketio.emit('success', (dict(bot.core.s.cookies.items()), bot.core.loginInfo['url'], nickname), room=sid)
+    def success_ack(bot, sessionID, nickname):
+        socketio.emit('success', (dict(bot.core.s.cookies.items()), bot.core.loginInfo['url'], nickname), room=sessionID)
 
-    def background_thread(sid, cache_path):
+    def background_thread(sid, sessionID):
 
         def qr_callback(uuid, status, qrcode):
             logger.info('%s %s', uuid, status)
             if status != '408':
-                socketio.emit('qr', (uuid, status), room=sid)
+                socketio.emit('qr', (uuid, status), room=sessionID)
 
         def logout_callback():
             with shelve.open('bot_status') as bot_status:
-                bot_status[cache_path] = False
-            if cache_path in bots:
-                logger.info('%s logged out!', bots[cache_path].self.name)
-                del bots[cache_path]
-            os.remove(cache_path)
-            socketio.emit('logout', room=sid)
+                bot_status[sessionID] = False
+            if sessionID in bots:
+                logger.info('%s logged out!', bots[sessionID].self.name)
+                del bots[sessionID]
+            os.remove(sessionID)
+            socketio.emit('logout', room=sessionID)
 
         def login_callback():
-            if hasattr(bots.get(cache_path, None), 'logout'):
-                bots[cache_path].logout()
+            if hasattr(bots.get(sessionID, None), 'logout'):
+                bots[sessionID].logout()
 
         try:
-            bot = EmotionBot(qr_callback=qr_callback, cache_path=cache_path, logout_callback=logout_callback, login_callback=login_callback)
-            bots[cache_path] = bot
+            bot = EmotionBot(qr_callback=qr_callback, cache_path=sessionID, logout_callback=logout_callback, login_callback=login_callback)
+            # socketio.server.enter_room(room=cache_path, sid=sid)
+            bots[sessionID] = bot
             with shelve.open('bot_status') as bot_status:
-                bot_status[cache_path] = bot.alive
-            logger.info('%s logged in, cache at %s', bot.self.name, cache_path)
-            success_ack(bot, sid, bot.self.name)
+                bot_status[sessionID] = bot.alive
+            logger.info('%s logged in, cache at %s', bot.self.name, sessionID)
+            success_ack(bot, sessionID, bot.self.name)
         except EmotionBot.TimeoutException as e:
             logger.warning('uuid=%s, status=%s, timeout', e.uuid, e.status)
-            socketio.emit('qr', (e.uuid, 'timeout'), room=sid)
+            socketio.emit('qr', (e.uuid, 'timeout'), room=sessionID)
 
-    if 'cache_path' not in session:
+    if 'sessionID' not in session:
         return
-    bot = bots.get(session['cache_path'], None)
+    join_room(room=session['sessionID'], sid=request.sid)
+    bot = bots.get(session['sessionID'], None)
     if hasattr(bot, 'alive') and bot.alive and hasattr(bot, 'self') and hasattr(bot.self, 'name') and bot.self.name:
         success_ack(bot, request.sid, bot.self.name)
     else:
-        socketio.start_background_task(background_thread, sid=request.sid, cache_path=session['cache_path'])
+        socketio.start_background_task(background_thread, sid=request.sid, sessionID=session['sessionID'])
 
 
 if __name__ == '__main__':
