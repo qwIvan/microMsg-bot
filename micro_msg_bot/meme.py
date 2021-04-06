2a83164b069d4e61bae81e1356cb0688229d4e40
Ivan <qwIvan@users.noreply.github.com>
2021-04-06 23:28:21 +0800

    去除不需要的代码以节省内存

--- micro_msg_bot/meme.py
+++ micro_msg_bot/meme.py
@@ -1,6 +1,5 @@
 import requests
 import shelve
-import js2py
 import time
 from bs4 import BeautifulSoup
 from functools import lru_cache
@@ -10,23 +9,6 @@ from .logger import logger
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
 session = requests.Session()
 session.headers['user-agent'] = user_agent
-
-
-def jschl(tag, domain='www.doutula.com'):
-    snippet, obj = '', None
-    for line in tag.splitlines():
-        if 's,t,o,p,b,r,e,a,k,i,n,g' in line and ' ' in line:
-            define = line.rsplit(' ', maxsplit=1)[-1]
-            if '=' in define and define.endswith(';'):
-                obj = define.split('=')[0]
-                snippet += define
-        if 'submit()' in line:
-            break
-        if obj:
-            for seg in line.split(';'):
-                if seg.startswith(obj) and '=' in seg:
-                    snippet += seg + ';'
-    return js2py.eval_js(snippet) + len(domain)
 
 
 def large_img(url):
@@ -42,14 +24,6 @@ def search(keyword):
     resp = session.get('https://www.doutula.com/search', params={'keyword': keyword})
 
     if resp.status_code != 200:
-        soup = BeautifulSoup(resp.text, 'lxml')
-        tag = soup.select_one('script').text
-        answer = jschl(tag)
-        form = soup.select_one('form')
-        params = dict([(i['name'], i.get('value', None)) for i in form.select('input')])
-        params['jschl_answer'] = answer
-        time.sleep(5)
-        session.get('https://www.doutula.com' + form['action'], params=params, allow_redirects=False)
         logger.info('www.doutula.com new session %s', session.cookies.get_dict())
         resp = session.get('https://www.doutula.com/search', params={'keyword': keyword})
 
